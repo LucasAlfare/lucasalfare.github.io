776be776c6fdb11e4958f74e9447627bada9d976
Lucas Sousa <god-lucas@hotmail.com>
2019-10-12 16:30:24 -0300

    adicionada interface do Pedro, para melhor utilização

---
+++ script.js
@@ -0,0 +1,39 @@
+const enviar = document.getElementById('enviar');
+const limpar = document.getElementById('limpar');
+const laudo = document.getElementById('laudo');
+const output = document.getElementById('output');
+const app = document.querySelector('.app');
+const body = document.querySelector('body');
+
+function handleButtonEnviar(e) {
+    e.preventDefault();
+    //  O preventDefault é obrigatório, em baixo dele pode colocar o código q quiser
+    // output.innerText = resultadoQueVoceQuer
+
+    const laud = laudo.value;
+    let r = "";
+    let l = getAcoes(laud);
+    for (let i = 0; i < l.length; i++) {
+        r += l[i] + "<br>";
+    }
+
+    output.innerHTML = r === "" ? "TUDO CERTO NOS LEUCO E PLAQ..." : r;
+}
+
+function handleButtonLimpar(e) {
+    e.preventDefault();
+    laudo.value = '';
+    output.innerHTML = '';
+}
+
+function handleOutputFoco(e) {
+    if (e.target === output) {
+        app.classList.toggle('focado');
+    } else {
+        app.classList.remove('focado');
+    }
+}
+
+output.addEventListener('click', handleOutputFoco);
+enviar.addEventListener('click', handleButtonEnviar);
+limpar.addEventListener('click', handleButtonLimpar);
