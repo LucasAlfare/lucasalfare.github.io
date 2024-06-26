16a913aabe37cc4c7c4f4a73542a98cdd852c741
LucasAlfare <god-lucas@hotmail.com>
2024-06-26 18:35:26 -0300

    experiments

---
+++ personal/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <span>This is my own github page created using Vite and React. :)</span>
+    </div>
+  )
+}
+
+export default App
