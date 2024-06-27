64bad55a67387c4c54ad41c9d1ea30c6da628500
LucasAlfare <god-lucas@hotmail.com>
2024-06-26 23:55:46 -0300

    experiments

--- root-sources/src/App.tsx
+++ root-sources/src/App.tsx
@@ -1,8 +1,12 @@
+import { Stack, Typography } from "@mui/material"
+
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <span>This is my own github page created using Vite and React. :)</span>
+      <Stack direction="column">
+        <Typography variant="h1">Hello!</Typography>
+        <Typography variant="body1">This is my own github page created using Vite and React. :)</Typography>
+      </Stack>
     </div>
   )
 }
