bdbe6474f7581202570afc7b4a860f5f135a1912
LucasAlfare <god-lucas@hotmail.com>
2024-06-30 23:03:35 -0300

    installed tailwind (no build)

--- root-sources/src/main.tsx
+++ root-sources/src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import './index.css';
+
+
+function Kkk() {
+  return (
+    <h1 className="underline">
+      Hello world!
+    </h1>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Kkk />
   </React.StrictMode>,
 )
